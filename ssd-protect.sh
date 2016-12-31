2b85c9fb43606d831b35b3a466307555600058f4
Decagon <Decagon@users.noreply.github.com>
2016-12-31 14:38:04 -0400

    Saves previous reading to file; add threshold alert

--- ssd-protect.sh
+++ ssd-protect.sh
@@ -1,11 +1,41 @@
 # grep for your hd (e.g. sda1) assumes 512kb blocks
 # based on http://serverfault.com/questions/238033/measuring-total-bytes-written-under-linux
 
-stat=$( awk '/sd/ {print $3"\t"$10 / 2 / 1024}' /proc/diskstats | sed 's/\s\+/ /g' | grep sda1 )
+# Configuration
 
-mbWritten=$(echo $stat | cut -d " " -f 2)
+# the name after /dev/ for your hdd (e.g. sda1) for the hardrive that you want to monitor
+# you can use the "disks" app in ubuntu to find this information or by running df -h
+hdd="sdb"
 
-echo "Megabytes written (all time): $mbWritten"
+# where to store ssd protect information
+prevMbDb="$HOME/.ssd-protect.txt"
+
+# how many mb can be written before you should be notified that something is up (must be a positive integer)
+warningThreshold=1
+
+# End configuration
+
+# create database if it doesn't exist
+touch "$prevMbDb"
+
+# get new and prev stats
+prevMb=$(cat "$prevMbDb")
+currMbRaw=$( awk '/sd/ {print $3"\t"$10 / 2 / 1024}' /proc/diskstats | sed 's/\s\+/ /g' | grep $hdd )
+mbWritten=$(echo $currMbRaw | cut -d " " -f 2)
+
+# save current stats
+echo "$mbWritten" > "$prevMbDb"
+
+deltaMb=$(($mbWritten-$prevMb))
+
+# check to see if threshold is exceeded
+echo "$deltaMb megabyte(s) have been written since last check, threshold is $warningThreshold mb"
+
+if [[ "$deltaMb" -gt "$warningThreshold" ]]
+then
+	# http://stackoverflow.com/questions/2990414/echo-that-outputs-to-stderr
+	(>&2 echo "Threshold disk writes has been exceeded on $(date)! Megabytes written: $deltaMb")
+fi
 
 # exercise for the reader: save value to file, and compare it to tomorrow to see how many mb's were written
 # if there is more than, say, 1024 * 40 then you might want to track down the app that's writing too much
